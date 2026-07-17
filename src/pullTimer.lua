0a1073bd8c2616c16fb7b491c61106f6f9ff69e7
Kami North <38385422+Nutik-Wulf@users.noreply.github.com>
2026-07-17 13:10:05 -0400

    notebook + ui 2.6.1

--- src/pullTimer.lua
+++ src/pullTimer.lua
@@ -1,14 +1,30 @@
 local pull = getObjectFromGUID("3037ac")
 local t = pull.getValue()
+local clock = 0
 
 function onLoad()
     tick()
 end
 
 function tick()
+    -- Clock Update
     t = pull.getValue()
     if t > 0 then
         self.setValue(t)
     end
-    Wait.time(tick, 0.1)
+
+    -- UI Update
+    clock = os.date("%H:%M:%S")
+    UI.setAttribute("time", "text", clock)
+
+    if t == 0 then
+        UI.setAttribute("timer", "color", "#FFFF33AA")
+    else
+        UI.setAttribute("timer", "color", "#FFFFFFAA")
+    end
+
+    UI.setAttribute("timer", "text", "Pull in "..t)
+    UI.setAttribute("timerBar", "percentage", t / 727.0 * 100)
+
+    Wait.time(tick, 1/10)
 end
